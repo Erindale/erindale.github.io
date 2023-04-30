c7a2ffee75860ef8cecdb2874e74a9d39c7205a3
Erindale Woodford <erindalewoodford@gmail.com>
2023-04-30 23:04:24 +0100

    Trying to fix gradient position

--- script.js
+++ script.js
@@ -4,13 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const links = document.querySelectorAll(".link");
   
     banner.addEventListener("mousemove", (e) => {
-      const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
-      const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
-  
-      bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
-      banner.style.setProperty('--gradient-x', `${e.clientX}px`);
-      banner.style.setProperty('--gradient-y', `${e.clientY}px`);
-    });
+        const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
+        const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
+        const rect = banner.getBoundingClientRect();
+        const relX = e.clientX - rect.left;
+        const relY = e.clientY - rect.top;
+      
+        bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+        banner.style.setProperty('--gradient-x', `${relX}px`);
+        banner.style.setProperty('--gradient-y', `${relY}px`);
+      });
+      
   
     links.forEach((link) => {
       link.addEventListener("mouseover", () => {
