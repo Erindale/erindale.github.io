31b51f1b0aab09c6b070d9abf1ef2db9b9bea84e
Erindale Woodford <erindalewoodford@gmail.com>
2023-04-30 23:32:44 +0100

    New icons, no gradients

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const banner = document.querySelector(".banner");
-  const bannerBg = document.querySelector(".banner-bg");
-  const links = document.querySelectorAll(".link");
-
-  banner.addEventListener("mousemove", (e) => {
-    const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
-    const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
-
-    bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
-    document.documentElement.style.setProperty('--mouse-x', `${e.clientX}px`);
-    document.documentElement.style.setProperty('--mouse-y', `${e.clientY}px`);
-  });
-
-  links.forEach((link) => {
-    link.addEventListener("mouseover", () => {
-      banner.classList.add("blur-banner");
-      banner.style.setProperty('--gradient-opacity', 1);
-    });
-
-    link.addEventListener("mouseout", () => {
-      banner.classList.remove("blur-banner");
-      banner.style.setProperty('--gradient-opacity', 0);
+    const banner = document.querySelector(".banner");
+    const bannerBg = document.querySelector(".banner-bg");
+    const links = document.querySelectorAll(".link");
+  
+    links.forEach((link) => {
+      link.addEventListener("mouseover", () => {
+        banner.classList.add("blur-banner", "dim-banner");
+      });
+  
+      link.addEventListener("mouseout", () => {
+        banner.classList.remove("blur-banner", "dim-banner");
+      });
     });
   });
-});
+  
