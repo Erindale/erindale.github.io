e6707cc18bb1b64a12849c7e631172b9c8901ae5
Erindale Woodford <erindalewoodford@gmail.com>
2023-04-30 23:14:42 +0100

    Trying to fix gradient offset

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const banner = document.querySelector(".banner");
-    const bannerBg = document.querySelector(".banner-bg");
-    const links = document.querySelectorAll(".link");
-  
-    banner.addEventListener("mousemove", (e) => {
-        const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
-        const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
-        const rect = banner.getBoundingClientRect();
-        const relX = e.clientX - rect.left;
-        const relY = e.clientY - rect.top;
-      
-        bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
-        banner.style.setProperty('--gradient-x', `${relX}px`);
-        banner.style.setProperty('--gradient-y', `${relY}px`);
-      });
-      
-  
-    links.forEach((link) => {
-      link.addEventListener("mouseover", () => {
-        banner.classList.add("blur-banner");
-        banner.style.setProperty('--gradient-opacity', 1);
-      });
-  
-      link.addEventListener("mouseout", () => {
-        banner.classList.remove("blur-banner");
-        banner.style.setProperty('--gradient-opacity', 0);
-      });
+  const banner = document.querySelector(".banner");
+  const bannerBg = document.querySelector(".banner-bg");
+  const links = document.querySelectorAll(".link");
+
+  banner.addEventListener("mousemove", (e) => {
+    const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
+    const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
+
+    bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+    document.documentElement.style.setProperty('--mouse-x', `${e.clientX}px`);
+    document.documentElement.style.setProperty('--mouse-y', `${e.clientY}px`);
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("mouseover", () => {
+      banner.classList.add("blur-banner");
+      banner.style.setProperty('--gradient-opacity', 1);
+    });
+
+    link.addEventListener("mouseout", () => {
+      banner.classList.remove("blur-banner");
+      banner.style.setProperty('--gradient-opacity', 0);
     });
   });
-  
+});
