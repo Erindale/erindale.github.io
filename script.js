fd83714f47f7169991f5665392056f21f73bcd46
Erindale Woodford <erindalewoodford@gmail.com>
2023-04-30 23:50:15 +0100

    Trying to fix blur still

--- script.js
+++ script.js
@@ -3,13 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const bannerBg = document.querySelector(".banner-bg");
     const links = document.querySelectorAll(".link");
   
+    banner.addEventListener("mousemove", (e) => {
+      const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
+      const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
+  
+      bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+    });
+  
     links.forEach((link) => {
       link.addEventListener("mouseover", () => {
-        banner.classList.add("blur-banner", "dim-banner");
+        banner.classList.add("blur-banner");
+        bannerBg.classList.add("dim-banner");
       });
   
       link.addEventListener("mouseout", () => {
-        banner.classList.remove("blur-banner", "dim-banner");
+        banner.classList.remove("blur-banner");
+        bannerBg.classList.remove("dim-banner");
       });
     });
   });
