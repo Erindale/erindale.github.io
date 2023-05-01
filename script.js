b35e1f975abf2648e23fa28cf27942f90e623ba3
Erindale Woodford <erindalewoodford@gmail.com>
2023-05-01 14:37:25 +0100

    Depth map 2

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const banner = document.querySelector(".banner");
-    const bannerBg = document.querySelector(".banner-bg");
-    const links = document.querySelectorAll(".link");
-  
-    banner.addEventListener("mousemove", (e) => {
-      const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
-      const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
-  
-      bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
-    });
+  const banner = document.querySelector(".banner");
+  const bannerBg = document.querySelector(".banner-bg");
+  const depthMap = document.querySelector(".depth-map");
+  const links = document.querySelectorAll(".link");
+
+  banner.addEventListener("mousemove", (e) => {
+    const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
+    const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
+    const depthOffsetX = offsetX * 2; // Adjust the multiplier to control the parallax effect strength
+    const depthOffsetY = offsetY * 2; // Adjust the multiplier to control the parallax effect strength
+
+    bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+    depthMap.style.backgroundPosition = `${depthOffsetX}px ${depthOffsetY}px`;
+  });
   
     links.forEach((link) => {
       link.addEventListener("mouseover", () => {
