d12b5d86f8e3db43789576b575484676a9719928
Erindale Woodford <erindalewoodford@gmail.com>
2023-05-01 14:42:03 +0100

    depth map 3

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
 document.addEventListener("DOMContentLoaded", () => {
   const banner = document.querySelector(".banner");
   const bannerBg = document.querySelector(".banner-bg");
-  const depthMap = document.querySelector(".depth-map");
+  const canvas = document.getElementById("depth-map");
+  const ctx = canvas.getContext("2d");
   const links = document.querySelectorAll(".link");
+
+  const mainImage = new Image();
+  mainImage.src = "images/erindale_rope_bridge_banner.webp";
+
+  const depthImage = new Image();
+  depthImage.src = "images/erindale_rope_bridge_banner_depth.webp";
+
+  depthImage.onload = () => {
+    canvas.width = depthImage.width;
+    canvas.height = depthImage.height;
+    ctx.drawImage(depthImage, 0, 0);
+  };
+
+  const getDepth = (x, y) => {
+    const pixelData = ctx.getImageData(x, y, 1, 1).data;
+    return (pixelData[0] + pixelData[1] + pixelData[2]) / 3;
+  };
+
+  const updateParallax = (offsetX, offsetY) => {
+    const depthMultiplier = 5; // Adjust the multiplier to control the parallax effect strength
+
+    bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
+    mainImage.style.left = `${offsetX}px`;
+    mainImage.style.top = `${offsetY}px`;
+
+    for (let y = 0; y < depthImage.height; y++) {
+      for (let x = 0; x < depthImage.width; x++) {
+        const depth = getDepth(x, y) / 255;
+        const xPos = x + offsetX * depth * depthMultiplier;
+        const yPos = y + offsetY * depth * depthMultiplier;
+        ctx.drawImage(mainImage, x, y, 1, 1, xPos, yPos, 1, 1);
+      }
+    }
+  };
 
   banner.addEventListener("mousemove", (e) => {
     const offsetX = (window.innerWidth / 2 - e.pageX) / 25;
     const offsetY = (window.innerHeight / 2 - e.pageY) / 25;
-    const depthOffsetX = offsetX * 2; // Adjust the multiplier to control the parallax effect strength
-    const depthOffsetY = offsetY * 2; // Adjust the multiplier to control the parallax effect strength
 
-    bannerBg.style.backgroundPosition = `${offsetX}px ${offsetY}px`;
-    depthMap.style.backgroundPosition = `${depthOffsetX}px ${depthOffsetY}px`;
+    updateParallax(offsetX, offsetY);
   });
   
     links.forEach((link) => {
